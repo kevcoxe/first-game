419b7f6a9d46b3f3e68a86d7665eacbd05a6c6b6
Kevin Coxe <kevcoxe@gmail.com>
2021-02-13 01:51:57 -0500

    move stuff out

--- js/modules/objects/tree.js
+++
@@ -1,8 +0,0 @@
-class Tree extends RenderableObject {
-    constructor (x = 0, y = 0) {
-        width = 100;
-        height = 100;
-        color = "green"
-        super(x, y, width, height, color)
-    }
-}
